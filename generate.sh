72838f5f2948f7d177a55fc639f8480d4bdd2a71
Hui Wang <huwang@redhat.com>
2018-11-23 14:19:08 +0800

    Initial licenses feature for JWS5

--- generate.sh
+++ generate.sh
@@ -1,19 +1,13 @@
 #!/usr/bin/env bash
 set -x
-dirs=("httpd-rhel" "httpd-sun" "httpd-win" \
-    "jsvc-rhel" "jsvc-sun" "jsvc-win" \
-    "openssl-rhel" "openssl-sun" "openssl-win")
 rm -rf products
 mkdir -p products
-for dir in "${dirs[@]}"
-do
-cp licenses/${dir}/licenses.xml \
+cp licenses/licenses.xml \
 	./eap-licenses-generator/src/main/resources/template/
 pushd eap-licenses-generator
 mvn clean package
 popd
-mkdir -p products/${dir}/licenses/
-cp licenses/${dir}/* products/${dir}/licenses/
+mkdir -p products/licenses/
+cp licenses/* products/licenses/
 cp eap-licenses-generator/target/licenses/* \
-    products/${dir}/licenses/
-done
+    products/licenses/
